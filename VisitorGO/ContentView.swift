6b65cd87c0d559d8dc1869b25bc0cb130f59da25
Shusuke Imamura <syusuke5103@gmail.com>
2024-11-18 22:00:37 +0900

    Initial Commit

---
+++ VisitorGO/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  VisitorGO
+//
+//  Created by shusuke imamura on 2024/11/18.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ VisitorGO/VisitorGOApp.swift
@@ -0,0 +1,17 @@
+//
+//  VisitorGOApp.swift
+//  VisitorGO
+//
+//  Created by shusuke imamura on 2024/11/18.
+//
+
+import SwiftUI
+
+@main
+struct VisitorGOApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
